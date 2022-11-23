a69ac6b9e14ba5192d992e4cc37dd797f532d85d
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-11-22 22:37:48 -0300

    fix: fix lint errors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@mui/material";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const AppRoutes = () => {
 
-    return (
-        <Routes>
-            <Route path="/" element={<Button variant="contained" color="primary">Test</Button>} />
-            <Route path="*" element={<Navigate to={"/"} />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Button variant="contained" color="primary">Test</Button>} />
+      <Route path="*" element={<Navigate to={'/'} />} />
+    </Routes>
+  );
 };
